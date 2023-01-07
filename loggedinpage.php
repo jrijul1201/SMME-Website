4e9353e1c573d111778ad9c45cc689a244dbf473
yashagg2001 <yashgupta.ve@gmail.com>
2023-01-07 18:11:27 +0530

    faculty login profile changes 1

--- loggedinpage.php
+++ loggedinpage.php
@@ -19,6 +19,12 @@ $userName = $_SESSION['user_name'];
     <!-- Favicons -->
     <link href="assets/img/download.png" rel="icon">
     <link href="assets/img/download.png" rel="apple-touch-icon">
+
+    <style>
+        input[type="file"]::-webkit-file-upload-button {
+            display:none;
+        }
+    </style>
 </head>
 
 <body>
@@ -44,21 +50,34 @@ $userName = $_SESSION['user_name'];
                 style="cursor:pointer;padding:10px 20px; color:white; background:black; border-radius:3px; text-decoration:none;">LOGOUT</a>
         </center>
     </div>
+    <h3>Personal Information</h3>
     <form action="data_update.php" method="post" enctype="multipart/form-data">
+        <label for="photo">
+            <img src="assets/img/team/<?php echo $arr[$index]["img"]; ?>" title="Click to upload new photo" alt="<?php echo $arr[$index]["img"]; ?>" style="width:15%;height:15%;border-radius:50%;cursor:pointer";><br>
+        </label>
+        <input type="file" id="photo" name="photo" accept="image/*" size="1000000"><br><br> 
+        
         <label for="fname">Name:</label>
         <input type="text" id="fname" name="name" value="<?php echo $arr[$index]["name"]; ?>"><br><br>
-        <label for="gslink">Google Scholar Link:</label>
-        <input type="url" id="gslink" name="gslink" value="<?php echo $arr[$index]["gs"]; ?>"><br><br>
-        <label for="irinsid">IRINS ID:</label>
-        <input type="text" id="irinsid" name="irinsid" value="<?php echo $arr[$index]["irins"]; ?>"><br><br>
-        <label for="phone">Phone:</label>
-        <input type="text" id="phone" name="phone" value="<?php echo $arr[$index]["phone"]; ?>"><br><br>
-        <label for="post">Post:</label>
+        
+        <label for="phone">Phone:</label> +91-1905-
+        <input type="text" id="phone" name="phone" size="6" value="<?php echo $arr[$index]["phone"]; ?>"><br><br>
+        
+        <label for="address">Address:</label>
+        <input type="text" id="address" name="address" size="30" value="<?php echo $arr[$index]["address"]; ?>"><br><br>
+        
+        <label for="post">Designation:</label>
         <input type="text" id="post" name="post" value="<?php echo $arr[$index]["post"]; ?>"><br><br>
-        <label for="speciality">Speciality:</label>
-        <input type="text" id="speciality" name="speciality" value="<?php echo $arr[$index]["speciality"]; ?>"><br><br>
-        <label for="photo">Select a file to upload:</label>
-        <input type="file" id="photo" name="photo" accept="image/*" size="1000000"><br><br>
+        
+        <label for="speciality">Specialisation:</label>
+        <input type="text" id="speciality" name="speciality" size="30" value="<?php echo $arr[$index]["speciality"]; ?>"><br><br>
+        
+        <label for="gslink">Google Scholar Link:</label>
+        <input type="url" id="gslink" name="gslink" size="30" value="<?php echo $arr[$index]["gs"]; ?>"><br><br>
+        
+        <label for="irinsid">IRINS ID:</label>
+        <input type="text" id="irinsid" name="irinsid" size="8" value="<?php echo $arr[$index]["irins"]; ?>"><br><br>
+        
         <input type="submit" value="Update Changes">
     </form>
 </body>
