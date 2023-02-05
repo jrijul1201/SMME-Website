61f635797834ed7da16ac456633fcd39925a3ecb
jrijul1201 <jrijul1201@gmail.com>
2023-02-05 11:02:29 +0530

    format workspace

--- upload.php
+++ upload.php
@@ -8,20 +8,19 @@ $facultyIndex = $_SESSION['faculty_index'];
 $data = file_get_contents('faculty.json');
 $arr = json_decode($data, true);
 
-if(isset($_POST['image']))
-{
-	$data = $_POST['image'];
-	$image_array_1 = explode(";", $data);
-	$image_array_2 = explode(",", $image_array_1[1]);
-	$data = base64_decode($image_array_2[1]);
+if (isset($_POST['image'])) {
+    $data = $_POST['image'];
+    $image_array_1 = explode(";", $data);
+    $image_array_2 = explode(",", $image_array_1[1]);
+    $data = base64_decode($image_array_2[1]);
     $target_dir = "assets/img/team/";
-    $old_image_path = $target_dir. $arr[$facultyIndex]["img"];
+    $old_image_path = $target_dir . $arr[$facultyIndex]["img"];
     $new_image_name = time() . $arr[$facultyIndex]["img"];
     $new_image_path = $target_dir . $new_image_name;
-	file_put_contents($new_image_path, $data);
+    file_put_contents($new_image_path, $data);
 
     //deleting old image
-    if(file_exists($old_image_path)){
+    if (file_exists($old_image_path)) {
         unlink($old_image_path);
     }
 
@@ -29,7 +28,7 @@ if(isset($_POST['image']))
     $arr[$facultyIndex]["img"] = $new_image_name;
     file_put_contents("faculty.json", json_encode($arr));
 
-	echo $new_image_path;
+    echo $new_image_path;
 }
 
 ?>
